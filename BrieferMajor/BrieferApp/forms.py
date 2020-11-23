d8e8fbbb8e85b3a32ec7fe3bad0dba8fd95cebb8
Apoorva2599 <apoorvaseth999@gmail.com>
2020-11-23 11:41:13 +0530

    Integration added

--- BrieferMajor/BrieferApp/forms.py
+++ BrieferMajor/BrieferApp/forms.py
@@ -4,4 +4,5 @@ class Text(forms.form):
        text = forms.CharField(max_length=500)
        in_lang = forms.CharField(max_length=500)
        out_lang = forms.CharField(max_length=500)
+       n = forms.CharField(max_length=500)
        
--- BrieferMajor/BrieferApp/views.py
+++ BrieferMajor/BrieferApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 import nltk
 import language_check #grammar check
 from textblob import TextBlob
@@ -13,12 +14,110 @@ import language_tool_python
 import pandas as pd
 from numpy import pi
 from googletrans import Translator, LANGUAGES
-
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
+import operator
 # Create your views here.
+def upload(file_name):
+    # file = open(file_name, "r")
+    # filedata = file.readlines()
+    
+    # return filedata
+    if request.method == 'POST':
+        upload_f = request.FILES['file']
+        data_file = open(upload_f,"r")
+        data = data_file.read()
+        return HttpResponseRedirect(request.path_info)
+    else:
+        return render(request, 'upload.html', {"flag":0})
+
+def split_sentence(data):
+    article = data.split(". ")
+    sentences = []
+
+    for sentence in article:
+        #print(sentence)
+        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+    sentences.pop() 
+    
+    return sentences
+
+def sentence_similarity(sent1, sent2, stopwords=None):
+    if stopwords is None:
+        stopwords = []
+ 
+    sent1 = [w.lower() for w in sent1]
+    sent2 = [w.lower() for w in sent2]
+ 
+    all_words = list(set(sent1 + sent2))
+ 
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+ 
+    # build the vector for the first sentence
+    for w in sent1:
+        if w in stopwords:
+            continue
+        vector1[all_words.index(w)] += 1
+ 
+    # build the vector for the second sentence
+    for w in sent2:
+        if w in stopwords:
+            continue
+        vector2[all_words.index(w)] += 1
+ 
+    return 1 - cosine_distance(vector1, vector2)
+ 
+def build_similarity_matrix(sentences, stop_words):
+    # Create an empty similarity matrix
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+ 
+    for idx1 in range(len(sentences)):
+        for idx2 in range(len(sentences)):
+            if idx1 == idx2: #ignore if both are same sentences
+                continue 
+            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+
+    return similarity_matrix
+
+
+def Summary(request):
+    if request.method == "POST":
+        text = request.POST.get('text')
+        top_n = int(request.POST.get('n'))
+        nltk.download("stopwords")
+        stop_words = stopwords.words('english')
+        summarize_text = []
+
+        # Step 1 - Read text anc split it
+        data = text
+        sentences = split_sentence(data)
+        # Step 2 - Generate Similary Martix across sentences
+        sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+        # Step 3 - Rank sentences in similarity martix
+        sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+        scores = nx.pagerank(sentence_similarity_graph)
+
+        # Step 4 - Sort the rank and pick top sentences
+        ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+        #print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+
+        for i in range(top_n):
+            summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+        # Step 5 - Offcourse, output the summarize texr
+        print("Summarize Text: \n", ". ".join(summarize_text))
+        summary = ". ".join(summarize_text)
+        return render(request,'Summary.html',{'output':summary,'text':text})
+    else:
+        return render(request,'Summary.html',context=None)
+
 def home(request):
     return render(request,'home.html',context=None)
-def Summary(request):
-    return render(request,'Summary.html',context=None)
+# def Summary(request):
+#     return render(request,'Summary.html',context=None)
 def About(request):
     return render(request,'About.html',context=None)        
 
@@ -77,25 +176,61 @@ def Optimize(request):
     if request.method=='POST' and request.POST.get("text"):
         #FREQUENT WORDS
         # text = "Hello my name is Apoorva. My name is also popo. My home is in Indore/Sagar."
+        tool = language_tool_python.LanguageTool('en-US')
         text=request.POST.get('text')
         t1=text
+        matches = tool.check(t1)
+        #print(matches)
+
+        my_mistakes = []
+        my_corrections = []
+        start_positions = []
+        end_positions = []
+        
+        for rules in matches:
+            if len(rules.replacements)>0:
+                start_positions.append(rules.offset)
+                end_positions.append(rules.errorLength+rules.offset)
+                my_mistakes.append(text[rules.offset:rules.errorLength+rules.offset])
+                my_corrections.append(rules.replacements[0])
+            
+        
+            
+        my_new_text = list(t1)
+        
+        
+        for m in range(len(start_positions)):
+            for i in range(len(text)):
+                my_new_text[start_positions[m]] = my_corrections[m]
+                if (i>start_positions[m] and i<end_positions[m]):
+                    my_new_text[i]=""
+            
+        my_new_text = "".join(my_new_text)
+
+        print(my_new_text)
+                
+        #FREQUENT WORDS 
         stop_words = set(stopwords.words('english')) #for removing is, an, in, etc
         tokenizer = nltk.RegexpTokenizer(r"\w+") #removing punctuatuons.
-        words = tokenizer.tokenize(text)
+        words = tokenizer.tokenize(t1)
 
-        filtered_sentence = [w for w in words if not w in stop_words]
+        filtered_sentence = [w for w in words if not w in stop_words and len(w)!=1]
 
         frequency = Counter(filtered_sentence)
         print(frequency)
-        #SYNONYMS
-        synonyms = []
 
-        for f in frequency:
+        n_items = dict( sorted(frequency.items(), key=operator.itemgetter(1),reverse=True)[:5])
+        #SYNONYMS
+        synonyms = {}
+
+        for f in n_items:
             for syn in wordnet.synsets(f): 
+                s = set()
                 for l in syn.lemmas(): 
-                    synonyms.append(l.name()) 
+                    s.add(l.name())
+                synonyms[f] = ','.join(list(s))
+
         print(synonyms)
-        
-        return render(request,'Optimize.html',{'words':synonyms,"fl_1":1,"text":t1})
+        return render(request,'Optimize.html',{'optimized':my_new_text,"fl_1":1,"text":text,'words':synonyms})
     else:
         return render(request,'Optimize.html',context=None)    
